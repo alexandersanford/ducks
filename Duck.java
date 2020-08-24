ae34b0f2b49003999acef11e0a1d70aeb45bbf42
alexandersanford <70177671+alexandersanford@users.noreply.github.com>
2020-08-24 17:23:01 -0600

    Add files via upload

--- Duck.java
+++ Duck.java
@@ -3,6 +3,10 @@ public abstract class Duck {
   public abstract void display();
 
   public abstract void quack();
+  
+  public void fly() {
+	  System.out.println("Duck has learned to fly!")
+  }
 
   public void swim() {
     System.out.println("Duck swam (all ducks float)");
